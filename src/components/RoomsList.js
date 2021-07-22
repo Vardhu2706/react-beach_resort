e58e635d5e2323722ee463efb1241d86beb03255
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-07-22 11:49:29 +0530

    Created RoomsList Component.

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,9 +1,27 @@
 // Importing Helpers
 import React from "react";
 
+// Importing Components
+import Room from "./Room";
+
 // Functional Component
-function RoomsList() {
-  return <div>Rooms List</div>;
+function RoomsList({ rooms }) {
+  if (rooms.length === 0) {
+    return (
+      <div className="empty-search">
+        <h3>No Rooms Found.</h3>
+      </div>
+    );
+  }
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map((item) => {
+          return <Room key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
 }
 
 // Default Export
